503840aa9e96bc2afbd5524f4af18b5cf0a84dbc
hazbib <hamzeh.zbib@pnnl.gov>
2016-11-01 09:20:55 -0700

    sum function

---
+++ R/sumft.R
@@ -0,0 +1,21 @@
+\name{sumsunftion}
+\alias{sumfunction}
+\title{Adding eight to a number}
+
+\arguments{
+  \item{x}
+}
+\value{
+  x
+}
+\description{
+  takes a number and adds eight to it
+}
+\examples{
+  sumfunction()
+}
+
+sumfunction <- function(x) {
+  x = x + 8
+}
+
---
+++ R/sumfunction.R
@@ -0,0 +1,11 @@
+#' Add eight to a number
+#'
+#' @param x A number
+#' @return the sum of x and eight
+#' @examples
+#' sumfunction(x)
+
+sumfunction <- function(x) {
+  x = x + 8
+}
+
